fe1c52e13d12792fac00b3ef633084b45122e402
patrikduch <duchpatrik@icloud.com>
2022-03-05 03:10:14 +0100

    Removal of SQL data initialization

--- dbs/postgres/init.sql
+++ dbs/postgres/init.sql
@@ -2,5 +2,3 @@ CREATE TABLE projectdetail(
     id INT NOT NULL PRIMARY KEY,
     projectname VARCHAR(50) NOT NULL
 );
-
--- INSERT INTO projectdetail(id, projectname) VALUES (1, 'Test project with Nest.js by Patrik Duch');
